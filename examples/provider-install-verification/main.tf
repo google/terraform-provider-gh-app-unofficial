77e4759cb65075cceec947ac3458b37953220925
pdewilde <pdewilde@google.com>
2026-06-09 14:51:40 -0700

    feat: add VS Code integrated local development and debugging workflow

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -1,9 +1,13 @@
 terraform {
   required_providers {
     ghapp = {
-      source = "registry.terraform.io/google/ghapp-unofficial"
+      source = "registry.terraform.io/google/gh-app-unofficial"
     }
   }
 }
 
 provider "ghapp" {}
+
+data "ghapp_installations" "all" {
+  target_org = "my-org-slug"
+}
