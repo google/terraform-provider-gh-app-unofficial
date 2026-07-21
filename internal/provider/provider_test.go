3c825da02272d7c3479ec602d123cade272fb811
zaheersufi <zaheerjunaidsufi@gmail.com>
2026-07-21 17:41:23 +0000

    chore: rebase onto fix

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -4,8 +4,8 @@
 package provider
 
 import (
-	"os"
 	"context"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/provider"
@@ -17,12 +17,6 @@ import (
 // testAccProtoV6ProviderFactories is used to instantiate a provider during acceptance testing.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"ghapp": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
 }
 
 func TestProvider(t *testing.T) {
